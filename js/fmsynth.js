c1f3910b268b4b9d7f4c628fe1c6e0cc5487425b
alakim <alakm@rambler.ru>
2018-11-05 11:58:23 +0300

    Spectrum calculation fixed

--- js/fmsynth.js
+++ js/fmsynth.js
@@ -3,14 +3,30 @@ export function bessel(k, I){
 }
 
 export function spectrum(c, m, I){
-	const res = [];
+	const byF = (a,b)=>Math.sign(a.f-b.f);
+
+	const s1 = [];
 	for(let k=0; k<=I+2; k++){
 		const J = bessel(k, I);
-		res.push({f:c+k*m, a:J});
-		if(k!=0)res.push({f:c-k*m,
+		s1.push({f:c+k*m, a:J});
+		if(k!=0)s1.push({f:c-k*m,
 			a:J*(k%2?-1:1)
 		});
 	}
 
-	return res.sort((a,b)=>Math.sign(a.f-b.f));
+	const mm = new Map();
+	for(let x,i=0; x=s1[i],i<s1.length; i++){
+		const f = x.f<0?-x.f:x.f;
+		const a = x.f<0?-x.a:x.a;
+		if(mm.has(f)){
+			mm.set(f, mm.get(f)+a);
+		}
+		else mm.set(f, a);
+	}
+
+	return Array
+		.from(mm.entries())
+		.map(x=>({f:x[0], a:Math.abs(x[1])}))
+		.sort(byF)
+	;
 }
